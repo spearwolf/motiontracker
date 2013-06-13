dbab9357f7e1916225617e562d72cafea0e3910f
Wolfger Schramm <wolfger@spearwolf.de>
2013-06-13 10:43:32 +0200

    mini refactorings

--- assets/js/motiontracker.js
+++ assets/js/motiontracker.js
@@ -1,32 +1,41 @@
 // Created 2013 by wolfger@spearwolf.de
+// --> https://github.com/spearwolf/motiontracker
+//
 (function(root){
 
-    var api = root.MotionTracker = {};
+    var api = root.MotionTracker = {}
+      , tiltLRValue
+      , tiltFBValue
+      ;
 
     api.smoothing = 24.0;
+
+    function isLandscapeOrientation() {
+        return Math.abs(window.orientation) === 90;
+    }
 
     function handleMotionEvent(e) {
 
         var accel = e.accelerationIncludingGravity
           , facingUp = -1
-          , currentValue
-          , firstValue
           ;
 
-        if (accel.z > 0) facingUp = 1;
-
-        if (typeof api.tiltLRValue === 'undefined') {
-            api.tiltLRValue = api.tiltLR;
-            api.tiltFBValue = api.tiltFB;
+        if (typeof tiltLRValue === 'undefined') {
+            tiltLRValue = api.tiltLR;
+            tiltFBValue = api.tiltFB;
         }
 
-        api.tiltLR = ((accel.x) / 9.81) * -90;
+        api.tiltLR = (accel.x / 9.81) * -90;
+        if (accel.z > 0) facingUp = 1;
         api.tiltFB = ((accel.y + 9.81) / 9.81) * 90 * facingUp;
 
-        if (typeof api.tiltLRValue === 'number') {
-            currentValue = api.tiltLR;
-            api.tiltLRValue += (currentValue - api.tiltLRValue) / api.smoothing;
-            api.smoothTiltLR = api.tiltLRValue;
+        if (typeof tiltLRValue === 'number') {
+
+            tiltLRValue += (api.tiltLR - tiltLRValue) / api.smoothing;
+            tiltFBValue += (api.tiltFB - tiltFBValue) / api.smoothing;
+
+            api.smoothTiltLR = tiltLRValue;
+            api.smoothTiltFB = tiltFBValue;
         }
     }
 
